4a5ff285a02ce3e1f58b96a87b83e38577dfbaf7
JOHNN BRYAN TEIXEIRA RODRIGUEZ VOZZI <johnn_vozzi@estudante.sc.senai.br>
2022-11-25 22:31:50 -0300

    hue

--- pa/src/br/com/senai/core/domain/Entrega.java
+++ pa/src/br/com/senai/core/domain/Entrega.java
@@ -3,10 +3,21 @@ package br.com.senai.core.domain;
 import java.time.LocalDate;
 
 public class Entrega {
-	
 	private int codigo;
 	
 	private LocalDate dataDeSaida;
+	
+	private LocalDate horaDeSaida;
+	
+	private LocalDate dataDeChegada;
+	
+	private LocalDate horaDeChegada;
+	
+	private double kmPercorrido;
+	
+	private double valorTotal;
+	
+	
 	
 	
 }
